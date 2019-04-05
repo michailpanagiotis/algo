630134a105160b14ee9503c7bf83b30538098ceb
Panos Michail <michailpanagiotis@gmail.com>
2019-04-05 13:42:32 +0300

    Comment

--- algo.js
+++ algo.js
@@ -1,3 +1,8 @@
+
+/* This function detects items in an array that are the sum of 2 other items in an array
+ * @param {Array} array An array of numeric values
+ * @return {Array<{Object}>} An array of results in the form { sum: <Number>, parts: [<Number>, <Number>] }
+ */
 export const detectSums = (array) => {
   return null;
 };
